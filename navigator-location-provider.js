1f207d9765a65bdf8e8ebe43eff8105dc9e6dc6e
Torsten Freyhall <torsten@freyhall.se>
2016-04-15 18:15:14 +0200

    navigator-location-provider: Added this provider to triplet-core repo

---
+++ navigator-location-provider.js
@@ -0,0 +1,39 @@
+var Location = require('./trip-models/location.js');
+
+var NavigatorLocationProvider = function NavigatorLocationProvider() {
+  this._watchId = undefined;
+};
+
+module.exports = NavigatorLocationProvider;
+
+NavigatorLocationProvider.prototype.start = function start(onLocation, onError) {
+  if (this._watchId !== undefined) { return; }
+
+  if (!navigator.geolocation) {
+    return onError({code: 2});
+  }
+
+  this._watchId = navigator.geolocation.watchPosition(
+    function(pos) {
+      onLocation(locationFromPosition(pos));
+    },
+    onError,
+    { enableHighAccuracy: true }
+  );
+};
+
+NavigatorLocationProvider.prototype.stop = function stop() {
+  if (this._watchId === undefined) { return; }
+
+  navigator.geolocation.clearWatch(this._watchId);
+  this._watchId = undefined;
+};
+
+function locationFromPosition(pos) {
+  return new Location({
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude,
+    timestamp: pos.timestamp,
+    accuracy: pos.coords.accuracy
+  });
+}
