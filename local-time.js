e44e5b434ab62ade0d5dc37e7e4925949af308ce
Torsten Freyhall <torsten@freyhall.se>
2016-06-05 20:30:03 +0200

    Removed saving of data to localStorage. Storage should be implemented in each app.

--- local-time.js
+++ local-time.js
@@ -1,13 +1,9 @@
-var Storage = require('./util/local-storage.js');
-
-var STORAGE_KEY = 'localTimeDiff';
 var ONE_MINUTE = 60000;
-var _diff = parseInt(Storage.get(STORAGE_KEY) || 0);
+var _diff = 0;
 
 function set(date) {
   var diff = Date.now() - date.getTime();
   _diff = (Math.abs(diff) > ONE_MINUTE) ? diff : 0;
-  Storage.set(STORAGE_KEY, _diff);
 }
 
 function get() {
